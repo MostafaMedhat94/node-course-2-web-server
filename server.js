ba7d4f51a1e1d4f577b304cafc20f65f62a2932d
mmedhat94 <mostafa.medhat994@gmail.com>
2018-07-05 10:24:24 +0200

    Fix middleware appendFile problem

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const port = process.env.PORT || 3000;
 // MIDDLEWARE FUNCTIONS
 // ====================
 handlebars.registerPartials(`${__dirname}/views/partials`);
+
 app.set('view engine', 'hbs');
+
 app.use((req, res, next) => {
 
     const now = new Date().toString();
     const log = `${now}: ${req.method} ${req.url}`
     console.log(log);
+
      fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('Unable to append to server.log.');
@@ -24,10 +27,11 @@ app.use((req, res, next) => {
 
     next();
 });
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-    next();
-})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+//     next();
+// });
 
 app.use(express.static(`${__dirname}/public`));
 
